e8f2a6d5d378dd50a9f135d290efec7eb5ab3b3f
sillas torres <sillas.zom@gmail.com>
2023-02-27 10:25:57 -0300

    Atualização da class Conta

--- conta/src/conta/TestaBanco.java
+++ conta/src/conta/TestaBanco.java
@@ -2,14 +2,23 @@ package conta;
 public class TestaBanco { 
     public static void main(String[] args) { 
         Cliente paulo = new Cliente(); 
-        paulo.setNome ("sillas Silveira");
-        paulo.setCpf ("222.222.222-22");
-        paulo.setProfissao ("programador");
+        paulo.setNome("paulo Silveira");
+        paulo.setCpf("222.222.222-22");
+        paulo.setProfissao("programador");
+        Cliente sillas = new Cliente(); 
+        sillas.setNome("sillas torres");
+        sillas.setCpf("491.929.898-61");
+        sillas.setProfissao("Analista de teste");
 
         Conta contaDoPaulo = new Conta(5444, 8444); 
         contaDoPaulo.depositar(100);
+        contaDoPaulo.getagencia();
+        contaDoPaulo.getnumero();
+        contaDoPaulo.setTitular(paulo);
+        System.out.println(contaDoPaulo.getTitular().getNome());
+        contaDoPaulo.setTitular(sillas);
+        System.out.println(contaDoPaulo.getTitular().getNome());
+        
 
-        //contaDoPaulo.titular = paulo; 
-       // System.out.println(contaDoPaulo.titular.nome);
     }
 }
